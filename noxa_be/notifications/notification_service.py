9b9218577fecb3d5f79d95a079550f8c8dbbf04e
hongnhung225 <tranthihongnhung2205@gmail.com>
2024-11-13 01:13:37 +0700

    fix error when merging code

--- noxa_be/notifications/notification_service.py
+++ noxa_be/notifications/notification_service.py
@@ -1,7 +1,7 @@
 import json
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
-from accounts.models import Notification
+from accounts.models import Notification, ParentProfile
 
 class NotificationService:
     @staticmethod
