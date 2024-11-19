ecc32c1b90c8e0b7800d4532155e0e62d81f5fcf
bin2009 <npbloc.cntt@gmail.com>
2024-11-19 23:47:38 +0700

    collectstatic

--- noxa_be/noxa_be/settings.py
+++ noxa_be/noxa_be/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,6 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
