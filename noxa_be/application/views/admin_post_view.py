7a0f3b7e38f33437fb625ffdb7a8d962d9860553
Makememyday <congthinhluong.new@gmail.com>
2024-11-27 22:57:18 +0700

    feat: allow admin to delete comment - change the content

--- noxa_be/application/views/admin_post_view.py
+++ noxa_be/application/views/admin_post_view.py
@@ -1,6 +1,7 @@
 import json
 from django.db.models import Q
 from django.core.cache import cache
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,6 +14,10 @@ from application.serializers.post_serializer import PostSerializer
 from accounts.models import Notification
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+
+from application.models import JobPostComment
+from application.serializers.comment_serializer import CommentSerializer
+from accounts.serializers.account_serializer import ParentProfileSerializer, TutorProfileSerializer, UserSerializer
 
 from .helper import PostHelper
 
@@ -73,4 +78,39 @@ class AdminPostView(APIView):
         }
         NotificationService.add_notification(post.parent_id, description, additional_information)
         post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class AdminCommentView(APIView):
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get (self, request, pk):
+        comment = get_object_or_404(JobPostComment, comment_id=pk)
+
+        user = comment.user_id
+        parent_or_tutor = None
+        if user.role == Role.PARENT:
+            parent_or_tutor = ParentProfile.objects.get(user_id=user)
+        else:
+            parent_or_tutor = TutorProfile.objects.get(user_id=user)
+
+        user_profile = {}
+        user_profile['id'] = str(user.user_id)
+        user_profile['username'] = user.username
+        user_profile['profilename'] = parent_or_tutor.parentname if user.role == Role.PARENT else parent_or_tutor.tutorname
+        user_profile['avatar'] = parent_or_tutor.avatar.url if parent_or_tutor.avatar else None
+
+        data = {
+            'comment_id': str(comment.comment_id),
+            'post_id': str(comment.post_id.post_id),
+            'user_id': str(comment.user_id.user_id),
+            'comment': comment.comment,
+            'created_at': comment.created_at,
+            'user': user_profile,
+        }
+        return Response(data)
+    
+    def delete(self, request, pk):
+        comment = get_object_or_404(JobPostComment, comment_id=pk)
+        comment.comment = 'Bình luận này đã bị xóa do vi phạm tiêu chuẩn cộng đồng'
+        comment.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
