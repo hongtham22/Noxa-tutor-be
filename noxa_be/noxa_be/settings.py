5400e308f77b5c875d53efa0a667f57a04cc4939
bin2009 <npbloc.cntt@gmail.com>
2024-12-03 08:29:07 +0700

    Test redis notifications

--- noxa_be/noxa_be/settings.py
+++ noxa_be/noxa_be/settings.py
@@ -96,21 +96,21 @@ ASGI_APPLICATION = 'noxa_be.asgi.application'
 
 AUTH_USER_MODEL = 'accounts.User'  
 
-# CHANNEL_LAYERS = {
-#     'default': {
-#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
-#         'CONFIG': {
-#             'hosts': [('127.0.0.1', 6379)],  # Replace with your Redis host and port
-#         },
-#     },
-# }
-
-# normal layer for testing 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer'
-    }
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],  # Replace with your Redis host and port
+        },
+    },
 }
+
+# # normal layer for testing 
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels.layers.InMemoryChannelLayer'
+#     }
+# }
  
 
 # Database
