80b7af8c34f0da1b6db52bf6c0d7f5c74e107889
bin2009 <npbloc.cntt@gmail.com>
2024-12-03 11:16:50 +0700

    Change allowed hosts

--- noxa_be/noxa_be/settings.py
+++ noxa_be/noxa_be/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-rv$16$s*!%*jgka^)x9xxt66g^2ijwh8h^ffgdj2&v%k!bpmlu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '142.93.120.238', '147.182.214.11']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '147.182.214.11']
 
 
 # Application definition
