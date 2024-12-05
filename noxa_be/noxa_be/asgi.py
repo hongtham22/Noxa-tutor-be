5eea2185b5ddf003b0ef60e20bda88e7568ef5cc
bin2009 <npbloc.cntt@gmail.com>
2024-12-05 08:06:54 +0700

    fix: set default Django settings module in ASGI configuration

--- noxa_be/noxa_be/asgi.py
+++ noxa_be/noxa_be/asgi.py
@@ -1,12 +1,12 @@
 # asgi.py
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noxa_be.settings")
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from notifications.jwtTokenMiddleware import UserIDMiddleware
 import notifications.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noxa_be.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
